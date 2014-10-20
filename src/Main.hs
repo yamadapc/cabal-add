4863763a1a20765ca0621eab8a58ed1c3d22eea7
yamadapc <tacla.yamada@gmail.com>
2014-10-20 03:07:24 -0400

    Refactor for error handling and tool honesty
    When no targets are provided, it warns the user about the actions being
    performed and adds the dependency to *all* targets, even though we know
    that's not what we want to do on a final implementation.
    
    When targets are provided, it tells the user the feature isn't
    implemented and asks for forgiveness with a sad face (":P") and a
    reasonable exit code.

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,16 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Control.Arrow (second)
-import Control.Monad (when)
-import Data.Maybe (mapMaybe)
-import Distribution.Simple.Utils (tryFindPackageDesc)
+import Control.Monad (foldM, forM_, when)
+import Data.Either (partitionEithers)
+import Distribution.Package (Dependency(..))
 import Distribution.PackageDescription hiding (options)
 import Distribution.PackageDescription.Parse (readPackageDescription)
 import Distribution.PackageDescription.PrettyPrint (writeGenericPackageDescription)
-import Distribution.Text (simpleParse)
+import Distribution.Simple.Utils (tryFindPackageDesc)
+import Distribution.Text (Text(..), simpleParse, display)
 import Distribution.Verbosity (silent)
 import System.Directory (getCurrentDirectory)
 import System.Exit (ExitCode(..), exitSuccess, exitWith)
@@ -38,20 +40,17 @@ main = do
     (desc, descPath) <- getDirPackageDesc cwd
     logInfo opts ("Using cabal manifest: " ++ makeRelative cwd descPath)
 
-    -- Find dependencies - TODO: Output errors on unparsed input, instead
-    --                     of this shitty solution
-    let deps = mapMaybe simpleParse depNames
-
-    -- Update the package description
-    let desc' = desc { condExecutables = map (second (addConstraints deps))
-                                             (condExecutables desc)
-                     }
-
-    -- Write the updated description to disk
-    writeGenericPackageDescription descPath desc'
-  where addConstraints toAdd condt =
-            condt { condTreeConstraints = toAdd ++ condTreeConstraints condt
-                  }
+    case partitionEithers $ map simpleParse' depNames of
+        -- TODO - refactor this soup
+        ([], deps) -> case optionBuildTargets opts of
+            [] ->
+              -- Ignore verbosity here, since this is important
+              foldM (\curDesc dep -> do
+                  putStrLn $ "Adding " ++ display dep ++ " to all targets"
+                  return $ addToAllTargets dep curDesc) desc deps >>=
+              writeGenericPackageDescription descPath
+            _ -> putStrLn "Unimplemented :P" >> exitWith (ExitFailure 1)
+        (errs, _) -> forM_ errs putStrLn >> exitWith (ExitFailure 1)
 
 -- |
 -- Gets a package description corresponding to a certain directory, parses
@@ -62,6 +61,30 @@ getDirPackageDesc dir = do
     desc <- readPackageDescription silent descPath
     return (desc, descPath)
 
+-- |
+-- An `Either` version of `Distribution.Text.simpleParse`.
+simpleParse' :: Text a => String -> Either String a
+simpleParse' s = case simpleParse s of
+    Just x  -> Right x
+    Nothing -> Left $ "Unable to parse " ++ s
+
+-- |
+-- Adds a dependency to all targets in a `GenericPackageDescription` and
+-- returns it updated
+addToAllTargets :: Dependency -> GenericPackageDescription -> GenericPackageDescription
+addToAllTargets dep desc =
+    -- Update the package description
+    desc { condLibrary     = fmap (addConstraint dep) (condLibrary desc)
+         , condExecutables = addConstraintToTupleList dep (condExecutables desc)
+         , condTestSuites  = addConstraintToTupleList dep (condTestSuites desc)
+         , condBenchmarks  = addConstraintToTupleList dep (condBenchmarks desc)
+         }
+  where addConstraintToTupleList d = fmap (second (addConstraint d))
+        addConstraint toAdd condt =
+            condt { condTreeConstraints = toAdd : condTreeConstraints condt }
+
+-- |
+-- Logs an information message depending on the `Options` verbosity.
 logInfo :: Options -> String -> IO ()
 logInfo opts x | optionVerbose opts = putStrLn x
                | otherwise          = return ()
