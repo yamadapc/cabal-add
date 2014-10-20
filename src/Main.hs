faee23d1869abb42ede1a515a22e88538bc8355c
yamadapc <tacla.yamada@gmail.com>
2014-10-20 04:27:07 -0400

    Add initial working --targets implementation
    The `--targets` flag now works as expected. I found a funny `Functor`
    generalization of the `addConstraint` helper function and used it. The
    code looks pretty but very cryptic. I don't understand it that well, to
    be honest. I'll need to discuss the code with someone else to think of
    what to do with it.
    
    The next step now is to make the added dependencies be of the currently
    installed version, or of the latest version on the package database
    whenever no dependency version is specified.
    
    Another "TODO", which is trivial, is to not add duplicated dependencies.
    This should be the least hard to implement, though it probably is a good
    idea to refactor what we currently have beforehand.

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Arrow (second)
 import Control.Monad (foldM, forM_, when)
 import Data.Either (partitionEithers)
 import Distribution.Package (Dependency(..))
@@ -66,13 +65,22 @@ simpleParse' s = case simpleParse s of
 actionAddDependencies :: Options -> [Dependency] ->
                          GenericPackageDescription -> FilePath -> IO ()
 actionAddDependencies opts deps desc descPath = case optionBuildTargets opts of
+    -- TODO
+    -- * Don't re-evaluate `display dep` for every iteration
+    -- * Don't repeat the `foldM ... >>= ...` code
+    -- * Consider adding all dependencies at once, since this is just
+    --   written like this because it'd be harder to add logging the other
+    --   way
     -- Ignore verbosity here, since this is important
     [] -> foldM (\curDesc dep -> do
               putStrLn $ "Adding " ++ display dep ++ " to all targets"
               return $ addToAllTargets dep curDesc) desc deps >>=
           writeGenericPackageDescription descPath
-    _  -> putStrLn "Unimplemented :P" >>
-          exitWith (ExitFailure 1)
+    ts -> foldM (\curDesc dep -> do
+              putStrLn $ "Adding " ++ display dep ++ " to targets " ++
+                         concatMap (++ " ") ts -- Ugly, I know...
+              return $ addToTargets ts dep curDesc) desc deps >>=
+          writeGenericPackageDescription descPath
 
 -- |
 -- Adds a dependency to all targets in a `GenericPackageDescription` and
@@ -81,14 +89,58 @@ addToAllTargets :: Dependency -> GenericPackageDescription ->
                    GenericPackageDescription
 addToAllTargets dep desc =
     -- Update the package description
-    desc { condLibrary     = fmap (addConstraint dep) (condLibrary desc)
-         , condExecutables = addConstraintToTupleList dep (condExecutables desc)
-         , condTestSuites  = addConstraintToTupleList dep (condTestSuites desc)
-         , condBenchmarks  = addConstraintToTupleList dep (condBenchmarks desc)
+    -- TODO - Ask people if this is readable. I only got to it because of the
+    --        type system.
+    -- (I had the idea for the first `fmap` generalization, but `fmap . fmap`
+    -- was more of an experiment that turned out to work)
+    desc { condLibrary     = addConstraintF  dep (condLibrary desc)
+         , condExecutables = addConstraintFF dep (condExecutables desc)
+         , condTestSuites  = addConstraintFF dep (condTestSuites  desc)
+         , condBenchmarks  = addConstraintFF dep (condBenchmarks  desc)
          }
-  where addConstraintToTupleList d = fmap (second (addConstraint d))
-        addConstraint toAdd condt =
-            condt { condTreeConstraints = toAdd : condTreeConstraints condt }
+
+-- |
+-- Adds a dependency to a set of targets in a `GenericPackageDescription` and
+-- returns it updated
+addToTargets :: [String] -> Dependency -> GenericPackageDescription ->
+                GenericPackageDescription
+addToTargets [] _ desc = desc
+addToTargets (t:ts) dep desc | t == "library" = addToTargets ts dep desc'
+  where desc' = desc { condLibrary = addConstraintF dep (condLibrary desc) }
+addToTargets (t:ts) dep desc = addToTargets ts dep desc'
+  -- I tried to make this the least confusing I could, but it still sucks.
+  -- There must be a cleaner, better way to write it
+  where guardedAddConstraint :: (String, CondTree v [Dependency] b) ->
+                                (String, CondTree v [Dependency] b)
+        guardedAddConstraint x = if fst x == t
+                                    then addConstraintF dep x
+                                    else x
+        guardedAddConstraintF = fmap guardedAddConstraint
+        exs' = guardedAddConstraintF (condExecutables desc)
+        tss' = guardedAddConstraintF (condTestSuites  desc)
+        bcs' = guardedAddConstraintF (condBenchmarks  desc)
+        desc' = desc { condExecutables = exs'
+                     , condTestSuites  = tss'
+                     , condBenchmarks  = bcs'
+                     }
+
+-- |
+-- Adds a dependency constraint to a build node
+addConstraint :: a -> CondTree v [a] a1 -> CondTree v [a] a1
+addConstraint toAdd condt =
+    condt { condTreeConstraints = toAdd : condTreeConstraints condt }
+
+-- |
+-- `fmap`ed version of `addConstraint`
+addConstraintF :: Functor f => a -> f (CondTree v [a] a1) ->
+                                    f (CondTree v [a] a1)
+addConstraintF d = fmap (addConstraint d)
+
+-- |
+-- Doubly `fmap`ed version of `addConstraint`
+addConstraintFF :: (Functor f1, Functor f2) => a -> f2 (f1 (CondTree v [a] a1)) ->
+                                                    f2 (f1 (CondTree v [a] a1))
+addConstraintFF d = fmap (addConstraintF d)
 
 -- |
 -- Logs an information message depending on the `Options` verbosity.
