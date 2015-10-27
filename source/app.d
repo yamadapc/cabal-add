a0034fb67779cb0d35e9ae1628e07bc7773c0618
yamadapc <tacla.yamada@gmail.com>
2015-10-27 14:36:27 +0100

    Make cabal-add a little smarter

--- source/app.d
+++ source/app.d
@@ -1,27 +1,55 @@
 import std.algorithm : countUntil;
+import std.array : array, empty;
 import std.conv : to;
-import std.file : getcwd;
+import std.digest.md;
+import std.file : SpanMode, dirEntries, getcwd;
 import std.functional : not;
-import std.path : buildPath;
-import std.stdio : File, stderr, stdin, writeln;
+import std.path : buildPath, baseName;
+import std.stdio : File, stderr, stdin, writeln, writefln;
 import std.uni : isWhite;
 
 int main(string[] args) {
-  if(args.length < 3) {
+  if(args.length < 2) {
     stderr.writeln("Usage: cabal-add <dependency> <cabal-file>");
     return 1;
+  } else if(args.length < 3) {
+    auto resolved = findCabalFile();
+
+    if(resolved is null) {
+      stderr.writeln("Usage: cabal-add <dependency> <cabal-file>");
+      stderr.writeln("Error: No <cabal-file> provided and couldn't find it.");
+      return 1;
+    }
+
+    writefln("Using cabal-file: %s", resolved.baseName);
+    args ~= resolved;
   }
+
   string dependency = args[1];
   string target = args[2];
   auto inputfile = File(target, "r");
   auto tmp = File.tmpfile();
 
+  MD5 oldMd5;
+  MD5 newMd5;
+
   foreach(line; inputfile.byLine) {
-    tmp.writeln(processLine(dependency, line));
+    auto newLine = processLine(dependency, line);
+    oldMd5.put(cast(ubyte[]) line);
+    newMd5.put(cast(ubyte[]) newLine);
+    tmp.writeln(newLine);
   }
 
   inputfile.close();
   tmp.rewind();
+
+  if(oldMd5.finish() == newMd5.finish()) {
+    stderr.writeln("Error: Nothing changed in the file");
+    return 1;
+  }
+
+  writefln("%s -> %s", oldMd5.finish().toHexString, oldMd5.finish().toHexString);
+  writefln("Dependency %s added", dependency);
 
   auto outputfile = File(target, "w");
 
@@ -30,6 +58,15 @@ int main(string[] args) {
   }
 
   return 0;
+}
+
+string findCabalFile() {
+  auto cwd = getcwd();
+  auto candidates = cwd.dirEntries("*.cabal", SpanMode.shallow).array;
+  if(candidates.empty) {
+    return null;
+  }
+  return candidates[0];
 }
 
 string processLine(string dependency, char[] line) {
